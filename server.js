0736c41eff911138a6bce32cbe7a1e5e96a24bdd
Arastun123 <ekbr03@gmail.com>
2024-01-23 16:04:09 +0400

    Send data to db from client side

--- server.js
+++ server.js
@@ -1,60 +1,76 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
 
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json({ extended: true }));
 
-
 const PORT = 3000;
 
-const newData = [
-    {
-        "id": 1,
-        "name": "Computer",
-        "count": 2,
-        "price": 2000,
-    },
-    {
-        "id": 2,
-        "name": "Phone",
-        "count": 1,
-        "price": 1800,
-    },
-    {
-        "id": 3,
-        "name": "Mouse",
-        "count": 3,
-        "price": 20,
-    },
-    {
-        "id": 4,
-        "name": "Monitor",
-        "count": 5,
-        "price": 500,
-    },
-];
-
-app.get('/api/data', (req, res) => {
-    res.json(newData);
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'firstSoft2023!',
+    database: 'appdb',
 });
 
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
+
+app.get('/api/data', (req, res) => {
+    const sql = 'SELECT * FROM invoice';
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(result);
+        }
+    });
+});
 app.post('/api/invoice', (req, res) => {
     console.log('Received Invoice Data:', req.body);
     const { date, number, customer, rowsData } = req.body;
-    newData.push(req.body)
-    console.log('Data Structure:', typeof res);
-    res.status(200).json({ message: 'Invoice data received successfully' });
+    // const quantity = parseInt(rowsData[0].count) || 0;
+    // const price = parseInt(rowsData[0].price) || 0;
+    console.log(rowsData[0].count);
+    const insertSql = 'INSERT INTO invoice (date, number, customer, product_name, quantity, price) VALUES (?, ?, ?, ?, ?, ?)';
+    const insertValues = [date, number, customer, rowsData[0].product_name, rowsData[0].quantity, rowsData[0].price];
+
+    db.query(insertSql, insertValues, (err, result) => {
+        if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            console.log('Inserted into database:', result);
+
+            res.status(200).json({ message: 'Invoice data received successfully' });
+        }
+    });
 });
 
+
 app.get('/api/invoice', (req, res) => {
-    res.send(newData);
+
+    const sql = 'SELECT * FROM invoice';
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://192.168.88.41:${PORT}`);
 });
-
-
-
