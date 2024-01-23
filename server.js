f8db5eeb5a89adc6e4b158cb18b97ee8adc03623
Arastun123 <ekbr03@gmail.com>
2024-01-23 17:21:59 +0400

    Console errors

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json({ extended: true }));
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
 
 db.connect(err => {
     if (err) {
-        console.error('Error connecting to MySQL database:', err);
+        console.error(err);
     } else {
         console.log('Connected to MySQL database');
     }
@@ -29,39 +29,33 @@ app.get('/api/data', (req, res) => {
     const sql = 'SELECT * FROM invoice';
     db.query(sql, (err, result) => {
         if (err) {
-            console.error('Error executing MySQL query:', err);
+            console.error(err);
             res.status(500).json({ error: 'Internal Server Error' });
         } else {
             res.json(result);
         }
     });
 });
+
 app.post('/api/invoice', (req, res) => {
-    console.log('Received Invoice Data:', req.body);
     const { date, number, customer, rowsData } = req.body;
-    console.log(rowsData[0].count);
     const insertSql = 'INSERT INTO invoice (date, number, customer, product_name, quantity, price) VALUES (?, ?, ?, ?, ?, ?)';
     const insertValues = [date, number, customer, rowsData[0].product_name, rowsData[0].quantity, rowsData[0].price];
-
     db.query(insertSql, insertValues, (err, result) => {
         if (err) {
-            console.error('Error executing MySQL query:', err);
+            console.error(err);
             res.status(500).json({ error: 'Internal Server Error' });
         } else {
-            console.log('Inserted into database:', result);
-
             res.status(200).json({ message: 'Invoice data received successfully' });
         }
     });
 });
 
-
 app.get('/api/invoice', (req, res) => {
-
     const sql = 'SELECT * FROM invoice';
     db.query(sql, (err, result) => {
         if (err) {
-            console.error('Error executing MySQL query:', err);
+            console.error(err);
             res.status(500).json({ error: 'Internal Server Error' });
         } else {
             res.json(result);
@@ -69,6 +63,4 @@ app.get('/api/invoice', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://192.168.88.41:${PORT}`);
-});
+app.listen(PORT, () => { console.log(`http://192.168.88.41:${PORT}`) });
