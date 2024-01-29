62ca615817cb279f2cbd0aec4299c2d003040136
Arastun123 <ekbr03@gmail.com>
2024-01-29 14:32:14 +0400

    Api adding

--- server.js
+++ server.js
@@ -27,20 +27,6 @@ db.connect(err => {
     }
 });
 
-app.post('/api/invoice', (req, res) => {
-    const { date, number, customer, product_name, quantity, price } = req.body;
-    const insertSql = 'INSERT INTO invoice (date, number, customer, product_name, quantity, price) VALUES (?, ?, ?, ?, ?, ?)';
-    const insertValues = [date, number, customer, product_name, quantity, price];
-    db.query(insertSql, insertValues, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-            res.status(200).json({ message: 'Invoice data received successfully' });
-        }
-    });
-});
-
 
 app.get('/api/invoice', (req, res) => {
     const sql = 'SELECT * FROM invoice';
@@ -66,21 +52,6 @@ app.get('/api/nomenklatura', (req, res) => {
     });
 });
 
-
-app.post('/api/nomenklatura', (req, res) => {
-    const { date, number, customer, rowsData } = req.body;
-    const insertSql = 'INSERT INTO nomenklatura (date, number, customer, product_name, quantity, price) VALUES (?, ?, ?, ?, ?, ?)';
-    const insertValues = [date, number, customer, rowsData[0].product_name, rowsData[0].quantity, rowsData[0].price];
-    db.query(insertSql, insertValues, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-            res.status(200).json({ message: 'Invoice data received successfully' });
-        }
-    });
-});
-
 app.get('/api/kontragent', (req, res) => {
     const sql = 'SELECT * FROM kontragent';
     db.query(sql, (err, result) => {
@@ -93,34 +64,6 @@ app.get('/api/kontragent', (req, res) => {
     })
 })
 
-app.post('/api/kontragent', (req, res) => {
-    const { name, phone_number, tin, address } = req.body;
-    const insertSql = 'INSERT INTO kontragent (name, phone_number, tin, address) VALUES (?, ?, ?, ?)';
-    const insertValues = [name, phone_number, tin, address ];
-    db.query(insertSql, insertValues, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        } else {
-            res.status(200).json({ message: 'Invoice data received successfully' });
-        }
-    });
-});
-
-app.post('/api/contract', (req,res) => {
-    const {name, number, date, type, company_name, comment} = req.body;
-    const insertSql = 'INSERT INTO (name, number, date, type, company_name, comment) VALUES ( ?, ?, ?, ?, ? )';
-    const insertValues = [name, number, date, type, company_name, comment]
-    db.query(insertSql, insertValues, (err, result) => {
-        if(err){
-            console.error(err);
-            res.status(500).json({ error: 'Insternal server error'})
-        }
-        else{
-            res.status(200).json( { message: 'Contarct data received successfully'})
-        }
-    })
-})
 
 app.get('/api/orders', (req, res) => {
     const sql = 'SELECT * FROM orders';
@@ -160,7 +103,6 @@ app.get('/api/casse_orders', (req, res) => {
     })
 })
 
-
 app.get('/api/category', (req, res) => {
     const sql = 'SELECT * FROM category';
     db.query(sql, (err, result) => {
@@ -187,4 +129,105 @@ app.get('/api/price', (req,res) => {
     })
 })
 
+app.post('/api/nomenklatura', (req, res) => {
+    const { date, number, customer, rowsData } = req.body;
+    const insertSql = 'INSERT INTO nomenklatura (date, number, customer, product_name, quantity, price) VALUES (?, ?, ?, ?, ?, ?)';
+    const insertValues = [date, number, customer, rowsData[0].product_name, rowsData[0].quantity, rowsData[0].price];
+    db.query(insertSql, insertValues, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.status(200).json({ message: 'Invoice data received successfully' });
+        }
+    });
+});
+
+app.post('/api/invoice', (req, res) => {
+    const { date, number, customer, product_name, quantity, price } = req.body;
+    const insertSql = 'INSERT INTO invoice (date, number, customer, product_name, quantity, price) VALUES (?, ?, ?, ?, ?, ?)';
+    const insertValues = [date, number, customer, product_name, quantity, price];
+    db.query(insertSql, insertValues, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.status(200).json({ message: 'Invoice data received successfully' });
+        }
+    });
+});
+
+app.post('/api/kontragent', (req, res) => {
+    const { name, phone_number, tin, address } = req.body;
+    const insertSql = 'INSERT INTO kontragent (name, phone_number, tin, address) VALUES (?, ?, ?, ?)';
+    const insertValues = [name, phone_number, tin, address ];
+    db.query(insertSql, insertValues, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.status(200).json({ message: 'Invoice data received successfully' });
+        }
+    });
+});
+
+app.post('/api/contract', (req,res) => {
+    const {name, number, date, type, company_name, comment} = req.body;
+    const insertSql = 'INSERT INTO contract (name, number, date, type, company_name, comment) VALUES ( ?, ?, ?, ?, ?, ? )';
+    const insertValues = [name, number, date, type, company_name, comment]
+    db.query(insertSql, insertValues, (err, result) => {
+        if(err){
+            console.error(err);
+            res.status(500).json({ error: 'Insternal server error'})
+        }
+        else{
+            res.status(200).json( { message: 'Contarct data received successfully'})
+        }
+    })
+})
+
+app.post('/api/routes', (req,res) => {
+    const { date, kontragentId, amount } = req.body;
+    const insertSql = 'INSERT INTO routes ( date, kontragentId, amount ) VALUES ( ?, ?, ? )';
+    const insertValues = [ date, kontragentId, amount ]
+    db.query(insertSql, insertValues, (err, result) => {
+        if(err){
+            console.error(err);
+            res.status(500).json({ error: 'Insternal server error'})
+        }
+        else{
+            res.status(200).json( { message: 'Contarct data received successfully'})
+        }
+    })
+})
+
+app.post('/api/orders', (req,res) => {
+    const { date, amount } = req.body;
+    const insertSql = 'INSERT INTO orders ( date, amount ) VALUES ( ?, ? )';
+    const insertValues = [ date, amount ]
+    db.query(insertSql, insertValues, (err, result) => {
+        if(err){
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error'})
+        }
+        else{
+            res.status(200).json( { message: 'Contarct data received successfully'})
+        }
+    })
+})
+
+app.post('/api/cassa_orders', (req, res) => {
+    const {date, kontragentId, amount} = rreq.body;
+    const insertSql = 'INSERT INTO casse_orders (date, kontragentId, amount) VALUES (?, ?, ?)';
+    const insertValues = [date, kontragentId, amount];
+    db.query(insertSql, insertValues, (err, result) => {
+        if(err){
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error'})
+        }
+        else {
+            res.status(200).json( { message: 'Date received'})
+        }
+    })
+})
 app.listen(PORT, () => { console.log(`http://192.168.190.57:${PORT}`) });
